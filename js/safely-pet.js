9a132198d439fd8d04aeb1a9b977c782f623c36b
Daniele <dinghino@gmail.com>
2015-09-23 02:16:33 +0200

    another one!
    * added most of the content (form and business adv missing)
    * wrote most of the html
    * header img and topbar almost complete (positioning of elements
    missing)
    * basic styling of text done (missing custom font fine tuning)
    * almost all the animations are missing
    * map disappeared... not sure why as the classes and ids are all there,
    still...
    * various that i don't remember being 2AM

--- js/safely-pet.js
+++ js/safely-pet.js
@@ -1,2 +1,23 @@
-$(documnent).ready(function () {
+$(document).ready(function () {
+	console.log("ready!");
+
+/* top bar script */
+
+myID = document.getElementById("topbar");
+var myScrollFunc = function () {
+    var y = window.scrollY;
+/*this sets the height when the classes switch */    
+    if (y >= 600) {
+        myID.className = "show"
+    } else {
+        myID.className = "hide"
+    }
+};
+
+window.addEventListener("scroll", myScrollFunc);
+
+/* end topbar script */
+
+
 });
+
