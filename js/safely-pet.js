5efd584950a9fdab017288ed67189105b3235a52
Lorenzo Berni <duplo@develer.com>
2015-09-30 23:38:27 +0200

    Handle right navigation dots

--- js/safely-pet.js
+++ js/safely-pet.js
@@ -1,22 +1,36 @@
 $(document).ready(function () {
 
   var sections = [];
+  var sectionTitles = [];
   $('.section').each(function () {
     var $this = $(this);
     sections.push($this.data('section-name'));
+    sectionTitles.push($this.data('section-title'));
   });
 
   // Initialize fullpage
   $('#fullpage').fullpage({
     anchors: sections,
+    navigation: true,
+    navigationPosition: 'right',
+    navigationTooltips: sectionTitles,
+    afterRender: function () {
+      var $navigation = $('#fp-nav');
+      $navigation.addClass('hidden-md');
+      $navigation.addClass('hidden-sm');
+      $navigation.addClass('hidden-xs');
+    },
     onLeave: function (index, nextIndex) {
-      var item = $('#topbar');
+      var $item = $('#topbar');
+      var $navigation = $('#fp-nav');
       if (index === 1) {
-        item.removeClass("up");
-        item.addClass("down");
+        $item.removeClass("up");
+        $item.addClass("down");
+        $navigation.addClass('visible');
       } else if (nextIndex === 1) {
-        item.removeClass("down");
-        item.addClass("up");
+        $item.removeClass("down");
+        $item.addClass("up");
+        $navigation.removeClass('visible');
       }
     }
   });
