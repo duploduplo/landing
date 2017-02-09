f450c18d11c3e390745ecd62cc58b86307019cec
Daniele Ding <dinghino@gmail.com>
2017-02-10 00:04:15 +0100

    new landing page stub
    first commit with the new landing page content

---
+++ assets/js/localization.js
@@ -0,0 +1,39 @@
+/**
+ * Redirect the browser to the correct localized page if available
+ * else default to english version
+ */
+
+/**
+ * Create the new path for the browser address
+ */
+function getLocalePath(lang) {
+  var _path
+
+  switch (lang) {
+    case "it":
+      _path = "/it/"
+      break
+    default:
+      _path = "/en/"
+  }
+  return _path
+}
+
+(function () {
+  // Get the browser language
+  lang = navigator.languages
+    ? navigator.languages[0]
+    : (navigator.language || navigator.userLanguage)
+
+  // get the first part of the language string so it can be used to redirect
+  // the browser to the localized content folder
+  lang = lang.split("-")[0]
+
+  // get the new path from the detected language (if any)
+  newPath = getLocalePath(lang)
+
+  // if a new path has been built redirect the browser there
+  if (newPath) {
+    window.location.replace(newPath)
+  }
+})()
