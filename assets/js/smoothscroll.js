69a294c978b0a55cc99385aa01ff79be38b64718
Daniele Ding <dinghino@gmail.com>
2017-02-13 14:51:57 +0100

    comment smoothscroll.js

--- assets/js/smoothscroll.js
+++ assets/js/smoothscroll.js
@@ -2,10 +2,19 @@
  * Enables smooth scrolling inside the page
  */
 $(function () {
+  /** Get all the links from
+   * #header (top menu)
+   * #banner (landing content)
+   * #main (page content)
+  */
   $headerLinks = $('#header a[href*="#"]:not([href="#"])')
   $bannerLinks = $('#banner a[href*="#"]:not([href="#"])')
   $pageLinks = $('#main a[href*="#"]:not([href="#"])')
 
+  /**
+   * scrollSmoth is our callback for clicking on the links/buttons that don't
+   * point to a blank `#` link
+   * */
   function scrollSmooth() {
     if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
       var target = $(this.hash);
@@ -13,12 +22,13 @@ $(function () {
       if (target.length) {
         $('html, body').animate({
           scrollTop: target.offset().top
-        }, 1000);
+        }, 1000); // speed in milliseconds
         return false;
       }
     }
   }
 
+  // add on click listeners
   $headerLinks.click(scrollSmooth);
   $bannerLinks.click(scrollSmooth);
   $pageLinks.click(scrollSmooth);
